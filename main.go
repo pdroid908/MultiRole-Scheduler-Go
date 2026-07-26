f0c1cbb8d8bdb361aa176a695b9cfc4fd724a156
pdroid908 <p1998nr@gmail.com>
2026-07-26 11:49:31 +0700

    konfigurasi dengan frontend

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.Use(func(c *gin.Context) {
-    c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Sesuaikan port frontend React kamu
+    c.Writer.Header().Set("Access-Control-Allow-Origin", "https://goojadwal.pages.dev/") // Sesuaikan port frontend React kamu
     c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
     c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
     c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
